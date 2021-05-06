75632e10401256b9a0e847295ea32359c5afc8aa
Kykal <benavides.alan@hotmail.com>
2021-05-06 07:09:17 -0500

    Almost finish

--- tspv4.py
+++ tspv4.py
@@ -50,19 +50,20 @@ route.append(initNumber+1)
 nodeBool[initNumber] = False
 
 #Multi-start with alpha
-for i in range( 0, 7 ) :
+for i in range( 0, dimension-1 ) :
     tempMinEucDistance = sys.float_info.max
     tempMaxEucDistance = 0
     tempNodes.clear()
     tempDistances.clear()
+    tempCoords.clear()
+    tempEuclideanDistances.clear()
     for j in range( 0, dimension ) : #Obtain all its euclidean distances from actual node
         x2 = nodes[j][0]
         y2 = nodes[j][1]
         euclideanDistance = Eu2D( x1, y1, x2, y2)
         
         tempEuclideanDistances.append(euclideanDistance)
-    
-    print( "\nDistances: %s" %tempEuclideanDistances)
+
     for j in range( 0, dimension ) :    #Looking for the smallest and greatest distance from my actual node to neighbors
         if (tempEuclideanDistances[j] > tempMaxEucDistance) and (nodeBool[j] == True) :
             tempMaxEucDistance = tempEuclideanDistances[j].copy()
@@ -71,33 +72,34 @@ for i in range( 0, 7 ) :
     
     djE = tempMinEucDistance + ( alpha*(tempMaxEucDistance-tempMinEucDistance) )
 
-    print("Coords: %s" %nodes)
-    print("djE: %s" %djE)
-
-    if sum(nodeBool) > 1 :
+    sumTrues = sum(nodeBool)
+    
+    if sumTrues > 1 :
         for j in range( 0, dimension ) :
             if ( tempEuclideanDistances[j] <= djE ) and ( nodeBool[j] == True ) :
                 tempDistances.append( tempEuclideanDistances[j] )
                 tempNodes.append( j )
                 tempCoords.append(nodes[j])
-    
-    nRand = rand.randint( 0, len(tempNodes)-1 )
-    print( "\nNodes: %s" %tempNodes)
-    print( "Distances: %s" %tempDistances)
-    print( "Coords: %s\n" %tempCoords)
-    print( "Random number: %s\n" %nRand)
-    print( "That node: %s" %tempNodes[nRand] )
-    nodeBool[ tempNodes[nRand] ] = False
-    print( "That distance: %s\n" %tempDistances[nRand] )
-    print( "That coord: %s" %tempCoords[nRand])
-    x1 = tempCoords[nRand][0]
-    y1 = tempCoords[nRand][1]
-    routeDistance += tempDistances[nRand]
+        nRand = rand.randint( 0, len(tempNodes)-1 )
+        nodeBool[ tempNodes[nRand] ] = False
+        x1 = tempCoords[nRand][0]
+        y1 = tempCoords[nRand][1]
+        routeDistance += tempDistances[nRand]
 
-    print(x1, y1)
+        route.append( tempNodes[nRand]+1 )
+    elif sumTrues == 1: 
+        for j in range( 0, dimension ) :
+            if nodeBool[j] == True :
+                routeDistance += Eu2D(x1, y1, nodes[j][0], nodes[j][1])
+                route.append( j+1 )
+                x1 = nodes[j][0]
+                y1 = nodes[j][1]
 
-    route.append( tempNodes[nRand]+1 )
+routeDistance += Eu2D( x1, y1, nodes[initNumber][0], nodes[initNumber][1] )
+route.append( initNumber+1 )
 
-print(nodeBool)
-print(route)
-print( "Distance: %s distance units" %routeDistance )
+execTime = time.time() - startTime
+
+print( "Best route: %s" %route)
+print( "Best distance: %s distance units" %routeDistance )
+print( "Time elapsed: %s seconds" %execTime)
