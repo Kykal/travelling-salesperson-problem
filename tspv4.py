0d5e5556202e802a64581cb4cb897de9dd77e583
Kykal <benavides.alan@hotmail.com>
2021-05-06 06:36:26 -0500

    Error last bool

--- tspv4.py
+++ tspv4.py
@@ -7,10 +7,10 @@ import random as rand
 #Declaring variables
 infoLoop = False
 nodes = []
-tempNodesDistances = []
-alpha = 0.4 #Greedy ಠ_ಠ
-tempConsiderDistances = []
-tempConsiderNodes = []
+tempEuclideanDistances = []
+alpha = 0.1 #Greedy ಠ_ಠ
+tempDistances = []
+tempNodes = []
 tempCoords = []
 route = []
 routeDistance = 0
@@ -50,47 +50,54 @@ route.append(initNumber+1)
 nodeBool[initNumber] = False
 
 #Multi-start with alpha
-for i in range( 0, dimension-2 ) :
-    tempNodesDistances.clear()
-    tempMinDistance = sys.float_info.max
-    tempMaxDistance = 0
-    djE = 0
-    tempConsiderDistances.clear()
-    tempConsiderNodes.clear()
-    tempCoords.clear()
-    amountConsiderNodes = 0
+for i in range( 0, 7 ) :
+    tempMinEucDistance = sys.float_info.max
+    tempMaxEucDistance = 0
+    tempNodes.clear()
+    tempDistances.clear()
     for j in range( 0, dimension ) : #Obtain all its euclidean distances from actual node
         x2 = nodes[j][0]
         y2 = nodes[j][1]
         euclideanDistance = Eu2D( x1, y1, x2, y2)
         
-        tempNodesDistances.append(euclideanDistance)
+        tempEuclideanDistances.append(euclideanDistance)
+    
+    print( "\nDistances: %s" %tempEuclideanDistances)
+    for j in range( 0, dimension ) :    #Looking for the smallest and greatest distance from my actual node to neighbors
+        if (tempEuclideanDistances[j] > tempMaxEucDistance) and (nodeBool[j] == True) :
+            tempMaxEucDistance = tempEuclideanDistances[j].copy()
+        if (tempEuclideanDistances[j] < tempMinEucDistance) and (nodeBool[j] == True) :
+            tempMinEucDistance = tempEuclideanDistances[j].copy()
+    
+    djE = tempMinEucDistance + ( alpha*(tempMaxEucDistance-tempMinEucDistance) )
 
-    if sum( nodeBool ) > 1 :
-        for j in range( 0, sum(nodeBool) ) :    #Obtain the smallest and greater distances between actual node and neighbors
-            if (tempNodesDistances[j] < tempMinDistance) and (tempNodesDistances[j] != 0) and ( nodeBool[j] == True ) :
-                tempMinDistance = tempNodesDistances[j]
-            if (tempNodesDistances[j] > tempMaxDistance) and ( nodeBool[j] == True ) :
-                tempMaxDistance = tempNodesDistances[j]
+    print("Coords: %s" %nodes)
+    print("djE: %s" %djE)
 
-        djE = ( tempMinDistance + ( alpha*( tempMaxDistance-tempMinDistance ) ) )
+    if sum(nodeBool) > 1 :
+        for j in range( 0, dimension ) :
+            if ( tempEuclideanDistances[j] <= djE ) and ( nodeBool[j] == True ) :
+                tempDistances.append( tempEuclideanDistances[j] )
+                tempNodes.append( j )
+                tempCoords.append(nodes[j])
+    
+    nRand = rand.randint( 0, len(tempNodes)-1 )
+    print( "\nNodes: %s" %tempNodes)
+    print( "Distances: %s" %tempDistances)
+    print( "Coords: %s\n" %tempCoords)
+    print( "Random number: %s\n" %nRand)
+    print( "That node: %s" %tempNodes[nRand] )
+    nodeBool[ tempNodes[nRand] ] = False
+    print( "That distance: %s\n" %tempDistances[nRand] )
+    print( "That coord: %s" %tempCoords[nRand])
+    x1 = tempCoords[nRand][0]
+    y1 = tempCoords[nRand][1]
+    routeDistance += tempDistances[nRand]
 
-        for j in range( 0, sum(nodeBool) ) :
-            if tempNodesDistances[j] <= djE and nodeBool[j] == True: 
-                tempConsiderDistances.append( tempNodesDistances[j] )
-                tempConsiderNodes.append( j )
-                amountConsiderNodes += 1
-        print( nodeBool )
-        print( "Amount of True: %s"  %sum(nodeBool))
-        print( amountConsiderNodes )
-        print( tempConsiderDistances)
-        nRand = rand.randint(0, amountConsiderNodes)
+    print(x1, y1)
 
-        route.append( tempConsiderNodes[nRand]+1 )
-        routeDistance += tempConsiderDistances[nRand]
+    route.append( tempNodes[nRand]+1 )
 
-        nodeBool[ tempConsiderNodes[nRand] ] = False
-
-        tempCoords = nodes[tempConsiderNodes[nRand]].copy()
-        x1 = tempCoords[0]
-        y1 = tempCoords[1]
+print(nodeBool)
+print(route)
+print( "Distance: %s distance units" %routeDistance )
