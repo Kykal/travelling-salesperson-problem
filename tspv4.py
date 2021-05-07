07592ca6770a22684934a7bebfcdec2f9d2a4786
Kykal <benavides.alan@hotmail.com>
2021-05-07 07:25:21 -0500

    Ready to go.

--- tspv4.py
+++ tspv4.py
@@ -13,10 +13,11 @@ tempNodes = []
 tempCoords = []
 route = []
 routeDistance = 0
+routeCoords = []
 realRoute = []
 realDistance = 0
 alpha = 0.1 #Greedy ಠ_ಠ
-timeLimit = 10.0 #seconds
+timeLimit = 600.0 #seconds
 bestDistance = sys.float_info.max
 timeLimitExceed = False
 
@@ -25,7 +26,7 @@ def Eu2D(x1, y1, x2, y2):
     return np.sqrt( (x1-x2)**2 + (y1-y2)**2 )
 
 #Open the file we want to use.
-tsp = open("TSP/ulysses16.tsp", "r")
+tsp = open("TSP/eil76.tsp", "r")
 
 #Save its information.
 while infoLoop == False :
@@ -33,7 +34,7 @@ while infoLoop == False :
     actualLineLenght = len(actualLine)
     if actualLine[0] == 'NAME' or actualLine[0] == 'NAME:' :
         filename = actualLine[actualLineLenght-1]
-        print( "\n%s.tsp file loaded!\n" %filename )
+        print( "\n%s file loaded!\n" %filename )
     elif actualLine[0] == 'DIMENSION' or actualLine[0] == 'DIMENSION:' :
         dimension = int( actualLine[actualLineLenght-1] )
         nodeBool = [ True for i in range(0, dimension) ]
@@ -54,6 +55,7 @@ x1 = nodes[initNumber][0]
 y1 = nodes[initNumber][1]
 route.append(initNumber+1)
 nodeBool[initNumber] = False
+routeCoords.append(initCoords)
 
 #Multi-start with alpha
 for x in range( 0, 3**dimension ) : 
@@ -70,6 +72,9 @@ for x in range( 0, 3**dimension ) :
     nodeBool[initNumber] = False
     realDistance = 0
     realRoute.clear()
+    routeCoords.clear()
+    routeCoords.append(initCoords)
+    lsDistance = 0
 
     for i in range( 0, dimension-1 ) :
         if actualTime >= timeLimit :
@@ -109,7 +114,7 @@ for x in range( 0, 3**dimension ) :
             x1 = tempCoords[nRand][0]
             y1 = tempCoords[nRand][1]
             routeDistance += tempDistances[nRand]
-
+            routeCoords.append(tempCoords[nRand])
             route.append( tempNodes[nRand]+1 )
         elif sumTrues == 1: 
             for j in range( 0, dimension ) :
@@ -118,27 +123,49 @@ for x in range( 0, 3**dimension ) :
                     route.append( j+1 )
                     x1 = nodes[j][0]
                     y1 = nodes[j][1]
+                    routeCoords.append( nodes[j] )
 
     routeDistance += Eu2D( x1, y1, nodes[initNumber][0], nodes[initNumber][1] )
     route.append( initNumber+1 )
+    routeCoords.append( initCoords )
+    realRouteCoords = routeCoords.copy()
 
     realRoute = route.copy()
     realDistance = routeDistance.copy()
 
-    
     if realDistance < bestDistance :
         bestDistance = realDistance.copy()
         bestRoute = realRoute.copy()
-        print("Cyle: {:,.0f}\tBest distance at the moment: {:,.5f}" .format(x+1, bestDistance))
+        print("Alpha notifiacion:\n\tCyle: {:,.0f}\tBest distance at the moment: {:,.5f}" .format(x+1, bestDistance))
+        print( "\tRoute: %s" %bestRoute)
+
+    #Local search (2node-switch)
+    for k in range(1, dimension-1) :
+        aux = realRouteCoords[k]
+        realRouteCoords[k] = realRouteCoords[k+1]
+        realRouteCoords[k+1] = aux.copy()
+
+        aux = realRoute[k]
+        realRoute[k] = realRoute[k+1]
+        realRoute[k+1] = aux
+
+        lsDistance = 0
+        for f in range(0, dimension) :
+            lsDistance += Eu2D( realRouteCoords[f][0], realRouteCoords[f][1], realRouteCoords[f+1][0], realRouteCoords[f+1][1]  )
+        
+        if lsDistance < bestDistance :
+            bestDistance = lsDistance.copy()
+            bestRoute = realRoute.copy()
+            print("Local Search notification:\n\tCyle: {:,.0f}\tBest distance at the moment: {:,.5f}" .format(x+1, bestDistance))
+            print( "\tRoute: %s\n" %realRoute)
     
     actualTime = time.time() - startTime
     if actualTime >= timeLimit :
         print( "\nTime limit exceeded [{:,.0f} second(s)]. Terminating program." .format(timeLimit) )
         break
 
-
 execTime = time.time() - startTime
 
 print( "\nBest route: %s" %bestRoute)
-print( "Best distance: %s distance units" %bestDistance )
-print( "Time elapsed: %s seconds" %execTime)
+print( "Best distance: {:,.2f} distance units" .format( bestDistance ) )
+print( "Time elapsed: %s seconds\n" %execTime)
