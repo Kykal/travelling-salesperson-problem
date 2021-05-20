ccf4f2de79db8fad77f776da8c33e9b6fa61c16b
Kykal <benavides.alan@hotmail.com>
2021-05-20 10:19:20 -0500

    K selector ready. temp/status need to be applicable

--- PIA/PIA.py
+++ PIA/PIA.py
@@ -1,13 +1,24 @@
 #Libraries
 from os import stat
+from typing import get_origin
 import numpy as np
 import sys
 import random
 
+from numpy.lib.function_base import select
+
 #Declaring variables
 dimension = 0
 coords = []
+coordsScore = []
 route = []
+routeCoords = []
+score = 0
+totalDistance = 0
+availableNodes = []
+availableCoords = []
+availableDistances = []
+k = 1
 
 #Function to calculate Euclidean Distance
 def Eu2D(x1, y1, x2, y2):
@@ -17,12 +28,6 @@ def Eu2D(x1, y1, x2, y2):
 path = "datasets/"
 filename = "set_64_1_15"
 filenamePath = path+filename+".txt"
-totalDistance = 0
-distance = 0
-minWorth = sys.float_info.max
-availableNodes = []
-availableCoords = []
-availableDistances = []
 
 tsp = open( filenamePath, 'r' )
 if filenamePath.split('_')[0] == 'datasets/set' :   #Check filename to know the dataset dimension
@@ -35,68 +40,56 @@ elif (filenamePath.split('_')[1] == 'problem') :
     if filenamePath.split('_')[2] == '3' :
         dimension = 33
 Tmax = tsp.readline().strip().split()[0]        #Save the available time budget per path as 'Tmax'
-dimension = int( dimension )
+dimension = int( dimension  ) - 1
 status = [ True for i in range(0, dimension-1) ]    #Set all nodes the status of 'True' (can be visited)
-for i in range(0, dimension) :      #Save all nodes coords and score in an array called 'coords'
+tempStatus = status.copy()
+for i in range(0, dimension+1) :      #Save all nodes coords and score in an array called 'coords'
     x,y,z = tsp.readline().strip().split()
     coords.append( [float(x), float(y), float(z)] )
 
 print( "\n"+filename+" file loaded!" )
+print( "From node 0 to %s" %dimension )
 
 #Save initial coordinates
 initCoord = coords[0]
 x1 = initCoord[0]
 y1 = initCoord[1]
-route.append(1)
-status[0] = False
-tempStatus = status.copy()
+xyRoute = [x1,y1]
+route.append(0)
+
+routeCoords.append( xyRoute )
 
 print( "\nInitial coords: [%s][%s]\n" %(x1, y1) )
-#Save the neighbor nodes that can be visited withouth exceed our Tmax
-if sum( status ) > 1 :
-    for i in range(2, dimension) :
-        x2 = coords[i][0]
-        y2 = coords[i][1]
-        distance = Eu2D( x1, y1, x2, y2 )
-        actualWorth = distance/coords[i][2]   #The least the better
-        if ( actualWorth <= minWorth ) and ( tempStatus[i] == True ) :
-            minWorth = actualWorth.copy()
-            minNode = i
-            minDistance = distance.copy()
 
-    availableNodes.append(minNode)
-    availableCoords.append(coords[minNode])
-    availableDistances.append(minDistance)
-    tempStatus[minNode] = False
-    x1 = coords[minNode][0]
-    y1 = coords[minNode][1]
+for i in range(0, 3) :
+    minDistance = sys.float_info.max
+    if sum( status ) > k :
+        for i in range(2, dimension+1) :    #Compare euc. distances from actual node to neighbor nodes to obtain the lowest euc. distance available 
+            x2 = coords[i][0]
+            y2 = coords[i][1]
 
-minWorth = sys.float_info.max
-for i in range(2, dimension) :
-    distance = Eu2D( x1, y1, x2, y2 )
-    actualWorth = distance/coords[i][2]   #The least the better
-    if ( actualWorth <= minWorth ) and ( tempStatus[i] == True ) :
-        minWorth = actualWorth.copy()
-        minNode = i
-        minDistance = distance.copy()
+            distance = Eu2D( x1, y1, x2, y2 )
+            if (distance < minDistance) and ( status[i] == True ) :
+                minDistance = distance
+                minNode = i
+        
+        status[minNode] = False
+        availableNodes.append( minNode )
+        availableCoords.append( coords[minNode] )
+        availableDistances.append( minDistance )
+        if k < 3 :
+            k += 1
+    else :
+        break
 
-availableNodes.append(minNode)
-availableCoords.append(coords[minNode])
-availableDistances.append(minDistance)
-tempStatus[minNode] = False
+randomNumber = random.randint( 0, k-1 )
+route.append( availableNodes[randomNumber] )
+xyRoute = [ availableCoords[randomNumber][0], availableCoords[randomNumber][1] ]
+routeCoords.append( xyRoute )
+totalDistance = availableDistances[randomNumber]
+score += coords[availableNodes[randomNumber]][2]
 
-minWorth = sys.float_info.max
-for i in range(2, dimension) :
-    distance = Eu2D( x1, y1, x2, y2 )
-    actualWorth = distance/coords[i][2]   #The least the better
-    if ( actualWorth <= minWorth ) and ( tempStatus[i] == True ) :
-        minWorth = actualWorth.copy()
-        minNode = i
-        minDistance = distance.copy()
-
-availableNodes.append(minNode)
-availableCoords.append(coords[minNode])
-availableDistances.append(minDistance)
-tempStatus[minNode] = False
-
-randNumber = random.randint()
+print( route )
+print( routeCoords )
+print( totalDistance )
+print( score )
